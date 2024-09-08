ecbbd3901113703c0a98716f7f0a5464c75054bb
morteza-7070 <mortezamemariansa70@gmail.com>
2024-09-09 00:22:37 +0330

    createProjectRestaurant

---
+++ project-restaurant/app/Http/Controllers/FastfoodCretishingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\fastfoodCretishing;
+use App\Http\Requests\StorefastfoodCretishingRequest;
+use App\Http\Requests\UpdatefastfoodCretishingRequest;
+
+class FastfoodCretishingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorefastfoodCretishingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(fastfoodCretishing $fastfoodCretishing)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(fastfoodCretishing $fastfoodCretishing)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatefastfoodCretishingRequest $request, fastfoodCretishing $fastfoodCretishing)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(fastfoodCretishing $fastfoodCretishing)
+    {
+        //
+    }
+}
---
+++ project-restaurant/database/migrations/2024_09_08_201242_create_fastfood_cretishings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fastfood_cretishings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('mime')->default('image/jpeg');
+            $table->string('description')->nullable();
+            $table->string('price');
+            $table->unsignedBigInteger('discount_id');
+            $table->foreign('discount_id')->references('id')->on('discounts')->onUpdate('cascade')->onDelete('cascade');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fastfood_cretishings');
+    }
+};
